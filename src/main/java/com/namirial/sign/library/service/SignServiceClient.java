93ef7625851e90fa644b162df880e1540217e454
Aldo Lushkja <a.lushkja@namirial.com>
2024-04-17 12:37:35 +0200

    add new basic auth system properties, add javadoc comments

--- src/main/java/com/namirial/sign/library/service/SignServiceClient.java
+++ src/main/java/com/namirial/sign/library/service/SignServiceClient.java
@@ -6,6 +6,7 @@ import it.pagopa.pn.library.exceptions.PnSpapiPermanentErrorException;
 import it.pagopa.pn.library.exceptions.PnSpapiTemporaryErrorException;
 import it.pagopa.pn.library.sign.pojo.PnSignDocumentResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import reactor.core.publisher.Mono;
@@ -19,25 +20,56 @@ import java.time.Duration;
 @Slf4j
 public class SignServiceClient {
 
+    // # =====================================
+    // # =        System properties          =
+    // # =====================================
     private static final String API_KEY_PROP = "namirial.server.apikey";
     private static final String API_ENDPOINT_PROP = "namirial.server.address";
+    private static final String USERNAME_PROP = "namirial.server.username";
+    private static final String PASSWORD_PROP = "namirial.server.password";
     private static final String MAX_CONNECTIONS_PROP = "namirial.server.max-connections";
-    public static final int DEFAULT_MAX_CONNECTIONS = 40;
     private static final String PENDING_ACQUIRE_TIMEOUT = "namirial.server.pending-acquire-timeout";
-    private static final int DEFAULT_PENDING_ACQUIRE_TIMEOUT = 600;
+
+    // # =====================================
+    // # =        Default values             =
+    // # =====================================
+    public static final int DEFAULT_MAX_CONNECTIONS = 40;
+    private static final int DEFAULT_PENDING_ACQUIRE_TIMEOUT = 600; // 10 minutes
+
+    // # =====================================
+    // # =        Constants                  =
+    // # =====================================
     private static final String API_KEY_HEADER_NAME = "X-SIGNBOX-EASYSIGN";
     private static final String REQUEST_ID_HEADER_NAME = "X-SIGNBOX-TRANSACTION-ID";
+    public static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+    public static final String BASIC_AUTH = "Basic ";
+
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    // # =====================================
+    // # =        Connection provider        =
+    // # =====================================
     private static final ConnectionProvider provider = ConnectionProvider.builder("custom")
                     .maxConnections(getMaxConnections())
                     .pendingAcquireTimeout(Duration.ofSeconds(getPendingAcquireTimeout()))
                     .pendingAcquireMaxCount(-1)
                     .build();
+
     private static final HttpClient httpClient = HttpClient.create(provider);
 
+    /**
+     * Sign a document using the Namirial SignBox service
+     * @param apiEndpoint The endpoint to use for the request
+     * @param requestId The request id
+     * @param data The data to sign as byte array
+     * @param format The format of the signature (e.g. CADES, PADES, XADES)
+     * @param level The level of the signature (e.g. BES, T)
+     * @return A {@link Mono} that will emit the response from the service
+     */
     public static Mono<PnSignDocumentResponse> sign(String apiEndpoint, String requestId, byte[] data, String format, String level) {
         return httpClient
                 .headers(h -> {
+                    h.set(AUTHORIZATION_HEADER_NAME, BASIC_AUTH + Base64.encodeBase64String((getUsername() + ":" + getPassword()).getBytes()));
                     h.set(API_KEY_HEADER_NAME, getApiKey());
                     h.set(REQUEST_ID_HEADER_NAME, requestId);
                 })
@@ -66,11 +98,23 @@ public class SignServiceClient {
                 .onErrorResume(SignServiceClient::resumeError);
     }
 
+    /**
+     * Parse the response from the service
+     * @param response The response from the service
+     * @param buffer The response body as byte array
+     * @param responseId The request id
+     * @return A {@link Mono} that will emit the response from the service
+     */
     private static Mono<PnSignDocumentResponse> parseResponse(HttpClientResponse response, byte[] buffer, String responseId) {
         log.info("Received response from requestId {} with status code: {}, reason: {}", responseId, response.status().code(), response.status().reasonPhrase());
         return Mono.just(new PnSignDocumentResponse(buffer));
     }
 
+    /**
+     * Resume from error
+     * @param t The error that occurred
+     * @return A {@link Mono} that will emit the error
+     */
     private static Mono<PnSignDocumentResponse> resumeError(Throwable t) {
         log.error("Resume from error with instanceof [{}]: {} ", ExceptionUtils.getRootCause(t).getClass().getCanonicalName(), t.getMessage());
         if (t instanceof PnSpapiPermanentErrorException) {
@@ -79,6 +123,13 @@ public class SignServiceClient {
         return Mono.error(new PnSpapiTemporaryErrorException(t.getMessage(), t));
     }
 
+    /**
+     * Get a temporary error
+     * @param response The response from the service
+     * @param buffer The response body as byte array
+     * @param responseId The response id
+     * @return A {@link Mono} that will emit the error
+     */
     private static Mono<PnSignDocumentResponse> getTemporaryError(HttpClientResponse response, byte[] buffer, String responseId) {
         log.error("Received temporary error status code {} from requestId {} with reason: {}", response.status().code(), responseId, response.status().reasonPhrase());
         ServerErrorResponse errorResponse = getServerErrorResponse(buffer);
@@ -88,6 +139,13 @@ public class SignServiceClient {
         return Mono.error(new PnSpapiTemporaryErrorException(response.status().reasonPhrase()));
     }
 
+    /**
+     * Get a permanent error
+     * @param response The response from the service
+     * @param buffer The response body as byte array
+     * @param responseId The response id
+     * @return A {@link Mono} that will emit the error
+     */
     private static Mono<PnSignDocumentResponse> getPermanentError(HttpClientResponse response, byte[] buffer, String responseId) {
         log.error("Received permanent status code {} from requestId {} with reason: {}", response.status().code(), responseId, response.status().reasonPhrase());
         ServerErrorResponse errorResponse = getServerErrorResponse(buffer);
@@ -97,6 +155,11 @@ public class SignServiceClient {
         return Mono.error(new PnSpapiPermanentErrorException(response.status().reasonPhrase()));
     }
 
+    /**
+     * Get the server error response
+     * @param buffer The response body as byte array
+     * @return The server error response
+     */
     private static ServerErrorResponse getServerErrorResponse(byte[] buffer) {
         ServerErrorResponse errorResponse = null;
         try {
@@ -112,27 +175,51 @@ public class SignServiceClient {
         return errorResponse;
     }
 
+    /**
+     * Get the API key
+     * @return The API key
+     */
     public static String getApiKey() {
-        return (System.getProperty(API_KEY_PROP) == null || System.getProperty(API_KEY_PROP).trim().isEmpty())
-                ? ""
-                : System.getProperty(API_KEY_PROP);
+        return StringUtils.isBlank(System.getProperty(API_KEY_PROP)) ? "" : System.getProperty(API_KEY_PROP);
     }
 
+    /**
+     * Get the API endpoint
+     * @return The API endpoint
+     */
     public static String getApiEndpoint() {
-        return (System.getProperty(API_ENDPOINT_PROP) == null || System.getProperty(API_ENDPOINT_PROP).trim().isEmpty())
-                ? ""
-                : System.getProperty(API_ENDPOINT_PROP);
+        return StringUtils.isBlank(System.getProperty(API_ENDPOINT_PROP)) ? "" : System.getProperty(API_ENDPOINT_PROP);
     }
 
+    /**
+     * Get the maximum number of connections
+     * @return The maximum number of connections
+     */
     public static Integer getMaxConnections() {
-        return (System.getProperty(MAX_CONNECTIONS_PROP) == null || System.getProperty(MAX_CONNECTIONS_PROP).trim().isEmpty())
-                ? DEFAULT_MAX_CONNECTIONS
-                : Integer.parseInt(System.getProperty(MAX_CONNECTIONS_PROP));
+        return StringUtils.isBlank(System.getProperty(MAX_CONNECTIONS_PROP)) ? DEFAULT_MAX_CONNECTIONS : Integer.parseInt(System.getProperty(MAX_CONNECTIONS_PROP));
     }
 
+    /**
+     * Get the pending acquire timeout
+     * @return The pending acquire timeout
+     */
     public static Integer getPendingAcquireTimeout() {
-        return (System.getProperty(PENDING_ACQUIRE_TIMEOUT) == null || System.getProperty(PENDING_ACQUIRE_TIMEOUT).trim().isEmpty())
-                ? DEFAULT_PENDING_ACQUIRE_TIMEOUT
-                : Integer.parseInt(System.getProperty(PENDING_ACQUIRE_TIMEOUT));
+        return StringUtils.isBlank(System.getProperty(PENDING_ACQUIRE_TIMEOUT)) ? DEFAULT_PENDING_ACQUIRE_TIMEOUT : Integer.parseInt(System.getProperty(PENDING_ACQUIRE_TIMEOUT));
+    }
+
+    /**
+     * Get the username
+     * @return The username
+     */
+    public static String getUsername(){
+        return StringUtils.isBlank(System.getProperty(USERNAME_PROP)) ? "" : System.getProperty(USERNAME_PROP);
+    }
+
+    /**
+     * Get the password
+     * @return The password
+     */
+    public static String getPassword(){
+        return StringUtils.isBlank(System.getProperty(PASSWORD_PROP)) ? "" : System.getProperty(PASSWORD_PROP);
     }
 }
