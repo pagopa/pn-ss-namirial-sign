f92ad5cbd191908cfafefaa2e4101a4dcb931999
Aldo Lushkja <161862100+alushkja@users.noreply.github.com>
2024-04-18 11:18:43 +0200

    Merge pull request #10
    * add new basic auth system properties, add javadoc comments

--- README.md
+++ README.md
@@ -13,17 +13,21 @@ This is a library to sign document using PAdES, XAdES and CAdES format.
 The library uses the following properties:
 - `namirial.server.address`: the URL of the Namirial Sign service
 - `namirial.server.apikey`: the API key to access the Namirial Sign service
+- `namirial.server.username`: the username to access the Namirial Sign service
+- `namirial.server.password`: the password to access the Namirial Sign service
 - `namirial.server.max-connections`: the number of maximum connections to the Namirial Sign service (default: 40)
 - `namirial.server.pending-acquire-timeout`: the timeout in seconds to acquire a connection to the Namirial Sign service (default: 600)
 
 ## Usage
 
 **Import the library in your project**
+
+${library.version} is available on pom.xml file of this project
 ```
 <dependency>
     <groupId>it.pagopa.pn</groupId>
     <artifactId>pn-ss-namirial-sign</artifactId>
-    <version>1.0.0-SNAPSHOT</version>
+    <version>${library.version}</version>
 </dependency>
 ```
 
@@ -32,9 +36,8 @@ The library uses the following properties:
 PnSignService signService = new PnSignServiceImpl();
 
 // Load content from file
-byte[] bytes = ...
-Mono<PnSignDocumentResponse> responseBes = signService
-        .signPdfDocument(bytes, false).flatMap(b -> consume signed content);
+byte[] bytes = Files.readAllBytes(Paths.get("path/to/file.pdf"));
+Mono<PnSignDocumentResponse> responseBes = signService.signPdfDocument(bytes, false).flatMap(b -> consume signed content);
 ```
 
 ## Build and install
--- src/main/java/com/namirial/sign/library/service/PnSignServiceImpl.java
+++ src/main/java/com/namirial/sign/library/service/PnSignServiceImpl.java
@@ -19,22 +19,46 @@ public class PnSignServiceImpl implements PnSignService {
 
     private String apiEndpoint;
 
-
+    /**
+     * Sign a PDF document with PAdES format
+     * @param fileBytes the PDF document to sign
+     * @param timestamping if true, the signature will be timestamped
+     * @return a {@link PnSignDocumentResponse} with the signed document
+     */
     @Override
     public Mono<PnSignDocumentResponse> signPdfDocument(byte[] fileBytes, Boolean timestamping) {
         return applySignature(SignatureFormat.PADES, timestamping, fileBytes);
     }
 
+    /**
+     * Sign an XML document with XAdES format
+     * @param fileBytes the XML document to sign
+     * @param timestamping if true, the signature will be timestamped
+     * @return a {@link PnSignDocumentResponse} with the signed document
+     */
     @Override
     public Mono<PnSignDocumentResponse> signXmlDocument(byte[] fileBytes, Boolean timestamping) {
         return applySignature(SignatureFormat.XADES, timestamping, fileBytes);
     }
 
+    /**
+     * Sign a generic document with CAdES format
+     * @param fileBytes the document to sign
+     * @param timestamping if true, the signature will be timestamped
+     * @return a {@link PnSignDocumentResponse} with the signed document
+     */
     @Override
     public Mono<PnSignDocumentResponse> pkcs7Signature(byte[] fileBytes, Boolean timestamping) {
         return applySignature(SignatureFormat.CADES, timestamping, fileBytes);
     }
 
+    /**
+     * Apply the signature to the document
+     * @param format the signature format
+     * @param timestamping if true, the signature will be timestamped
+     * @param fileBytes the document to sign
+     * @return a {@link PnSignDocumentResponse} with the signed document
+     */
     private Mono<PnSignDocumentResponse> applySignature(String format, Boolean timestamping, byte[] fileBytes) {
         if(fileBytes == null || fileBytes.length == 0) {
             return Mono.error(new PnSpapiPermanentErrorException("fileBytes cannot be null or empty"));
